33a6d7ed73dd2d9485e6aaedd171507e7e4441b8
SeFajardo <s.fajardog@uniandes.edu.co>
2026-05-28 22:36:25 -0500

    CAMBIO TF

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,11 +5,13 @@ variable "aws_region" {
 variable "key_pair_name" {
   description = "Nombre del key pair existente en AWS para acceso SSH"
   type        = string
+  default     = "vockey"
 }
 
 variable "github_repo_url" {
   description = "URL HTTPS del repo público con el código"
   type        = string
+  default     = "https://github.com/SeFajardo/Sprint4-Arquisoft.git"
 }
 
 variable "resource_prefix" {
